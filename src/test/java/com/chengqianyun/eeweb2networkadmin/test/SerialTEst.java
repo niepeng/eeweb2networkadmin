25661fd7ca305cc378e10a4e28a8f18efbe272cb
niepeng <niepeng>
2018-08-22 15:55:31 +0800

    add send sms  test.

--- src/test/java/com/chengqianyun/eeweb2networkadmin/test/SerialTEst.java
+++ src/test/java/com/chengqianyun/eeweb2networkadmin/test/SerialTEst.java
@@ -1,10 +1,12 @@
 package com.chengqianyun.eeweb2networkadmin.test;
 
+import com.chengqianyun.eeweb2networkadmin.core.utils.ShortMessageUnit;
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -18,16 +20,21 @@ import java.util.List;
 
 public class SerialTest {
 
+  static int MAX_SEND_LEN = 70;			// 单条短信最长的发送字数(70个)
+  static int SMS_SEND_INTERVAL =5*1000;	// 短信发完后等待间隔(5000毫秒)
   static int baudrate = 9600;
   static long waitMillTime = 500;
   static int PACKET_LENGTH = 500;
 
-  public static void main(String[] args) {
-    List<String> list = getAllComPorts();
-    System.out.println("list Size= " + list.size());
-    for (String tmp : list) {
-      System.out.println("value===>" + tmp);
-    }
+  public static void main(String[] args) throws Exception {
+//    List<String> list = getAllComPorts();
+//    System.out.println("list Size= " + list.size());
+//    for (String tmp : list) {
+//      System.out.println("value===>" + tmp);
+//      callPhone(tmp, "15372095699");
+//      System.out.println("\n\n");
+//    }
+    callPhone("COM3", "15372095699");
   }
 
   public static void callPhone(String commName, String phone) throws Exception {
@@ -80,11 +87,17 @@ public class SerialTest {
       /**
        * 测试打电话
        */
-      flag = sendCommand(in,out,"ATD"+phone+";\r");
-      System.out.println("打电话结果:" + flag);
+//      flag = sendCommand(in,out,"ATD"+phone+";\r");
+//      System.out.println("打电话结果:" + flag);
+//
+//      Thread.sleep(30 * 1000);
+//      sendCommand(in,out,"ATH\r");
 
-      Thread.sleep(15 * 1000);
-      sendCommand(in,out,"ATH;\r");
+      Thread.sleep(2 * 1000);
+      System.out.println("\n 准备发送短信:");
+//      flag = sendMessage(URLDecoder.decode("13800571500", "utf-8"), URLDecoder.decode("15372095699", "utf-8"), URLDecoder.decode("我是短信内容,发送试一下", "utf-8"), out, in);
+      flag = sendMessage("13800571500", "15372095699", "helloworld.1234", out, in);
+      System.out.println("短信发送结果:" + flag);
 
       Thread.sleep(2 * 1000);
       close(serialPort);
@@ -104,8 +117,14 @@ public class SerialTest {
     if (out != null) {
       try {
         out.close();
-        in.close();
         out = null;
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    if (in != null) {
+      try {
+        in.close();
         in = null;
       } catch (IOException e) {
         e.printStackTrace();
@@ -123,9 +142,9 @@ public class SerialTest {
   }
 
   private static boolean sendCommand(InputStream in, OutputStream out, String content) throws IOException, InterruptedException {
-    write("ATE0\r",out);
+    write(content, out);
     boolean flag = read(in);
-    System.out.println("firstFlag=" + flag);
+    System.out.println(content + ",currentFlag=" + flag);
     return flag;
   }
 
@@ -153,7 +172,7 @@ public class SerialTest {
   public static String readPackData(InputStream in) {
     StringBuilder result = new StringBuilder();
     byte[] readBuffer = new byte[PACKET_LENGTH];
-    int numBytes = 0;
+    int numBytes;
     if (in == null) {
       return null;
     }
@@ -180,7 +199,7 @@ public class SerialTest {
   public static List<String> getAllComPorts() {
     List<String> comList = new ArrayList<String>();
     Enumeration en = CommPortIdentifier.getPortIdentifiers();
-    CommPortIdentifier portIdRs = null;
+    CommPortIdentifier portIdRs;
 
     while (en.hasMoreElements()) {
       portIdRs = (CommPortIdentifier) en.nextElement();
@@ -190,6 +209,73 @@ public class SerialTest {
     }
     return comList;
   }
+
+  /**
+   * 给手机号码为phoneNo发送msg的短消息. 返回:true:成功  false:失败
+   * @param centerNo 短信中心号
+   * @param phoneNo 手机号
+   * @param message 发送短信的内容
+   */
+  public static boolean sendMessage(String centerNo, String phoneNo, String message, OutputStream out, InputStream in) {
+    boolean rsbool = true;
+    String msg ="";					// 经过处理的,要发送的短信内容
+    String sendmsg4At = "";			// 经过编码后,要发送的信息
+    int msgLen = message.length();	// 短信内容的中长度(太长会被分几次发送)
+    int len = 0;					// 单次发送的总长度
+    int beginIndex = 0; 			// 截取字符串的的其实位置
+    int subLen = 0;					// 临时变量
+    int index = 0;
+    boolean hasNext = true;
+    try {
+      subLen = msgLen;
+      while (hasNext) {
+        // 截取短信内容
+        if (subLen > MAX_SEND_LEN){
+          subLen = MAX_SEND_LEN;
+        }else{
+          hasNext = false;
+        }
+        msg = message.substring(index, index + subLen);
+        index = index + subLen;
+        subLen = msgLen - index;
+
+        // 发送短信内容
+        //centerNo = "+86" + centerNo;
+        sendmsg4At = ShortMessageUnit.code4sendMessage(centerNo, phoneNo, msg);// 发送短信AT指令需要的字符串
+        len = sendmsg4At.length();
+        beginIndex = Integer.parseInt(sendmsg4At.substring(0, 2),16) * 2 + 2;		// 短信中心所占的长度
+//				System.out.println("index:"+index+"\nsubLen:"+(index+subLen)+"\nmsg:"+msg);
+//				System.out.println("AT+CMGS=" + ((len - beginIndex)/2));
+//				System.out.println(sendmsg4At);
+//				System.out.println(msg);
+
+
+
+          String content1 = "AT+CMGS=" + ((len - beginIndex)/2) + "\r";
+          String content2 = sendmsg4At + "\r";
+          String content3 = (char)26 + "\r";
+//        write("AT+CMGS=" + ((len - beginIndex)/2) + "\r", out);
+//        write(sendmsg4At + "\r", out);											// ② 发送AT指令(发送短信内容)
+//        write((char)26 + "\r", out);												// ③ 发送AT指令(发送结束符号""(即(char)26))
+
+        System.out.println("content1==>" + content1);
+        System.out.println("content2==>" + content2);
+        System.out.println("content3==>" + content3);
+        write(content1, out);
+        write(content2, out);
+        write(content3, out);
+
+        Thread.sleep(SMS_SEND_INTERVAL);											// ④发完短信等待SMS_SEND_INTERVAL毫秒
+        // sendATCommand("AT\r");
+        //rsbool = receiveATResponse(RESPONSE_OK,RESPONSE_ERROR,1)!=null;				// 读取模块响应结果 -- 可以看到结果 -- 测试用
+        rsbool = read(in); // 读取模块响应结果 -- 正式用
+      }
+    } catch (Exception e) {
+      rsbool = false;
+    }
+    return rsbool;
+  }
+
 
 
 }
