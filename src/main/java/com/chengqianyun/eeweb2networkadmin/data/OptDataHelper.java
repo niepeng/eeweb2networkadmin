f6fbe60d0d84f9e410384009f615123062f03e8e
niepeng <niepeng>
2019-05-15 17:27:55 +0800

    fix bug: alarm reset conditon alarm_end_time lost.

--- src/main/java/com/chengqianyun/eeweb2networkadmin/data/OptDataHelper.java
+++ src/main/java/com/chengqianyun/eeweb2networkadmin/data/OptDataHelper.java
@@ -403,7 +403,9 @@ public class OptDataHelper {
     }
 
     if (dataIntime.getInStatus() == StatusEnum.normal.getId() || dataIntime.getInStatus() == 0) {
-      resetAlarmAll(dataIntime);
+      if(dataIntime.getInStatus() == StatusEnum.normal.getId()) {
+        resetAlarmAll(dataIntime);
+      }
       return;
     }
 
